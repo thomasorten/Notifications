cceb006c4c1271c31a88edd897071fc0a9f3bf15
thomasorten <thomasorten@Thomass-MacBook-Pro.local>
2014-06-05 10:48:35 -0500

    Initial Commit

---
+++ NotificationsTests/NotificationsTests.m
@@ -0,0 +1,34 @@
+//
+//  NotificationsTests.m
+//  NotificationsTests
+//
+//  Created by Thomas Orten on 6/5/14.
+//  Copyright (c) 2014 Orten, Thomas. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NotificationsTests : XCTestCase
+
+@end
+
+@implementation NotificationsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
